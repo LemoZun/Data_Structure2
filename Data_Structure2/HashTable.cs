5901fedd23af55ba0e310f7ecb3a911264e425b6
LemoZun <marrzun@gmail.com>
2024-08-01 11:57:22 +0900

    docs : 해시테이블과 딕셔너리

---
+++ Data_Structure2/HashTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure2
+{    
+    internal class HashTable
+    {
+        static void Main()
+        {
+            // 해시테이블 기반의 dictionary 자료구조
+            Dictionary<string, Monster> monsterDic = new Dictionary<string, Monster>();
+
+            // 삽입
+            // Add(키값 , 실제 value)
+            monsterDic.Add("피카츄", new Monster("피카츄", MonsterType.Electric, 80));
+            monsterDic.Add("파이리", new Monster("파이리", MonsterType.Fire, 90));
+            monsterDic.Add("꼬부기", new Monster("꼬부기", MonsterType.Water, 70));
+            monsterDic.Add("이상해씨", new Monster("이상해씨", MonsterType.Electric, 100));
+            monsterDic["피죤"] = new Monster("피죤", MonsterType.Wind, 50); // 이렇게도 가능
+            
+            // try도 사용 가능
+            // monsterDic.TryAdd
+            // monsterDic.TryGetValue
+            
+
+            // 삭제
+            // Remove(키값)
+            monsterDic.Remove("이상해씨");
+
+            // 탐색
+            // 딕셔너리[키값]
+            Monster find1 = monsterDic["피카츄"]; // O(1)
+            Console.WriteLine($"{find1.name},{find1.type},{find1.hp}");
+
+            if(monsterDic.ContainsKey("이상해씨")) // 있는지 없는지 확인하는 메서드
+            {
+                Monster find2 = monsterDic["이상해씨"];
+            }
+            monsterDic["피카츄"].hp = 110; // 키값(인덱서)를 통해 값 변경 가능 
+        }
+        public enum MonsterType { Fire, Water, Grass, Electric, Wind }
+        public class Monster
+        {
+            public string name;
+            public MonsterType type;
+            public int hp;
+
+            public Monster(string name, MonsterType type, int hp)
+            {
+                this.name = name;
+                this.type = type;
+                this.hp = hp;
+            }
+        }
+    }    
+}
---
+++ Data_Structure2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data_Structure2
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
